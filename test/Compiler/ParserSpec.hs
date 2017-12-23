712e6b5d31cf81f3d801453df66fe9422b15f359
Jonas Benn <mail@benn.in>
2017-12-23 18:00:56 +0100

    Add language pragma and fix test
    - Add `DuplicateRecordFields`-pragma to the whole project
      Fixes #16
    - Fix and reformat test cases

--- test/Compiler/ParserSpec.hs
+++ test/Compiler/ParserSpec.hs
@@ -11,9 +11,21 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "program" $ do
-     it "parses an empty class" $ do
-       (parseTestString "class Test {}") `shouldBe` Just [(Class (Identifier "Test") [] [])]
-  describe "objpgrm" $ do
-     it "parses an object class" $ do
-       (parseTestString "class Objectclass {void objectMethod(){new Objectclass();}}") `shouldBe` Just [Class (Identifier "Smallobjectclass") [] [Method {getIdentifier = Identifier "objectMethod",getMods = [],getReturnType = JVoid,getParamList = [], getBody = Just (Block [StmtExprStmt (Instantiation (Name {path = [], getIdentifier = Identifier "Smallobjectclass"}) [])])}]]
+  describe "Parser" $ do
+     it "parses an empty class" $ parseTestString "class Test {}"
+       `shouldBe`
+       Just [Class (Identifier "Test") [] []]
+     it "parses an object class" $ parseTestString "class Objectclass {void objectMethod(){new Objectclass();}}"
+       `shouldBe`
+       Just [Class (Identifier "Objectclass") []
+               [Method { getIdentifier = Identifier "objectMethod"
+                       , getMods = []
+                       , getReturnType = JVoid
+                       , getParamList = []
+                       , getBody = Just (
+                           Block [StmtExprStmt
+                                  (Instantiation
+                                   Name { path = []
+                                        , getIdentifier = Identifier "Objectclass"}
+                                    []
+                                  )])}]]
