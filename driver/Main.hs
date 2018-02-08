b8e1b2cb31da6539311115a5dd186f465506fc98
Jonas Benn <mail@benn.in>
2018-02-08 16:04:28 +0100

    Driver improved
    - Has now a verbose flag

--- driver/Main.hs
+++ driver/Main.hs
@@ -23,31 +23,33 @@ import           Turtle.Prelude
 main :: IO ()
 main = run
 
-cliParser :: Parser (Maybe T.FilePath, Bool)
-cliParser = (,) <$>
+cliParser :: Parser (Maybe T.FilePath, Bool, Bool)
+cliParser = (\ a b c -> (a,b,c)) <$>
   (optional (argPath "FilePath" "Path to .java-file") <|> pure Nothing)
   <*> switch "type-only" 't' "If this flag is set, the compiler will only type-check the code."
+  <*> switch "verbose" 'v' "If this flag is set, the final result will be output on stdout."
 
 run :: IO ()
 run =
   do
-    (filePath,typeCheckFlag) <- options "Tiny Java Compiler" cliParser
+    (filePath,typeCheckFlag, verboseFlag) <- options "Tiny Java Compiler" cliParser
     input <- getInput filePath
     let inputName = fromMaybe "StdIn" $ show <$> filePath
     if typeCheckFlag
-      then parseAndTypecheck inputName input
-      else fullCompilation inputName input
+      then parseAndTypecheck inputName input verboseFlag
+      else fullCompilation inputName input verboseFlag
   where
-    fullCompilation :: String -> T.Text -> IO ()
-    fullCompilation inputName input =
+    fullCompilation :: String -> T.Text -> Bool -> IO ()
+    fullCompilation inputName input verboseFlag =
       do
         let compilationResult = runFullPipeline inputName (Text.unpack input)
-        either printError (handleSuccess inputName) compilationResult
-    parseAndTypecheck :: String -> T.Text -> IO ()
-    parseAndTypecheck inputName input =
+        either printError (handleSuccess inputName verboseFlag) compilationResult
+    parseAndTypecheck :: String -> T.Text -> Bool -> IO ()
+    parseAndTypecheck inputName input verboseFlag =
       do
         let compilationResult = runUntilTypecheck inputName (Text.unpack input)
-        either printError (const $ printSuccessMessage inputName "type-checked") compilationResult
+        either printError (handleTypeCheckSuccess inputName verboseFlag) compilationResult
+        -- if verboseFlag then printf ("Typed AST"%w%) compilationResult else return ()
 
 printError :: Error -> IO ()
 printError = eprintf (errorTextLeft%w%errorTextRight)
@@ -58,35 +60,47 @@ printError = eprintf (errorTextLeft%w%errorTextRight)
 type ClassFileName = String
 type Result = [(ClassFileName,ClassFile)]
 
-handleSuccess :: String -> Result -> IO ()
-handleSuccess inputName results = do
+handleTypeCheckSuccess :: String -> Bool -> [(String,Class)] -> IO ()
+handleTypeCheckSuccess inputName verboseFlag results = do
+  printSuccessMessage inputName "type-checked"
+  if verboseFlag
+    then mapM_ (\(name,res) -> printf("\nClass Name: "%w%"\nByte-code:\n"%w%"\n") name res) results
+    else return ()
+
+handleSuccess :: String -> Bool -> Result -> IO ()
+handleSuccess inputName verboseFlag results = do
   mapM_ (uncurry generateByteFile) results
   printSuccessMessage inputName "compiled"
+  if verboseFlag
+    then mapM_ (\(name,res) -> printf("\nClass Name: "%w%"\nByte-code:\n"%w%"\n") name res) results
+    else return ()
 
 printSuccessMessage :: String -> String ->  IO ()
 printSuccessMessage inputName jobName = printf ("=== Success ===\n\n~~~\n"%s%" was successfully "%s%"!\n~~~\n") (Text.pack inputName) (Text.pack jobName)
 
-runUntilTypecheck :: String -> String -> Either Error [Class]
+runUntilTypecheck :: String -> String -> Either Error [(String,Class)]
 runUntilTypecheck filePath input =
   do
     nonEmptyInput <- if null input
                        then Left $ ParseError "The input appears to be empty."
                        else return input
     parseResult <- parseSrc filePath  nonEmptyInput
-    typecheck parseResult
+    typeCheckResult <- typecheck parseResult
+    return $ zip (map getClassName typeCheckResult) typeCheckResult
+  where
+    getClassName :: Class -> ClassFileName
+    getClassName (Class (Identifier className) _ _ ) = className ++ ".class"
+    getClassName _ = error "Partial function"
 
 runFullPipeline :: String -> String -> Either Error Result
 runFullPipeline filePath input =
   do
     typeCheckResult <- runUntilTypecheck filePath input
-    compilationResult <- mapM compileByteCode typeCheckResult
-    return $ zip (map getClassName typeCheckResult) compilationResult
+    compilationResult <- mapM (compileByteCode . snd) typeCheckResult
+    return $ zip (map fst typeCheckResult) compilationResult
   where
     compileByteCode :: Class -> Either Error ClassFile
     compileByteCode = const $ Left $ InternalError "Byte-code-generation failed!"
-    getClassName :: Class -> ClassFileName
-    getClassName (Class (Identifier className) _ _ ) = className ++ ".class"
-    getClassName _ = error "Partial function"
 
 getInput :: Maybe T.FilePath -> IO T.Text
 getInput (Just filePath) = readTextFile filePath
