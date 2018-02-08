9e38cdeff4723daf4853ccebf4cc0a0ee20f7aa3
Jonas Benn <mail@benn.in>
2018-02-08 16:28:32 +0100

    Add -p flag

--- driver/Main.hs
+++ driver/Main.hs
@@ -23,33 +23,33 @@ import           Turtle.Prelude
 main :: IO ()
 main = run
 
-cliParser :: Parser (Maybe T.FilePath, Bool, Bool)
-cliParser = (\ a b c -> (a,b,c)) <$>
+cliParser :: Parser (Maybe T.FilePath, Bool, Bool, Bool)
+cliParser = (\ a b c d -> (a,b,c,d)) <$>
   (optional (argPath "FilePath" "Path to .java-file") <|> pure Nothing)
+  <*> switch "parse-only" 'p' "If this flag is set, the compiler will only type-check the code."
   <*> switch "type-only" 't' "If this flag is set, the compiler will only type-check the code."
   <*> switch "verbose" 'v' "If this flag is set, the final result will be output on stdout."
+
+type Pipeline result = String -> String -> Either Error result
+type SuccessHandler result = String -> Bool -> result -> IO ()
 
 run :: IO ()
 run =
   do
-    (filePath,typeCheckFlag, verboseFlag) <- options "Tiny Java Compiler" cliParser
+    (filePath,parserFlag,typeCheckFlag,verboseFlag) <- options "Tiny Java Compiler" cliParser
     input <- getInput filePath
     let inputName = fromMaybe "StdIn" $ show <$> filePath
-    if typeCheckFlag
-      then parseAndTypecheck inputName input verboseFlag
-      else fullCompilation inputName input verboseFlag
+    if parserFlag
+      then run untilParser (handleSuccess "parsed") inputName input verboseFlag
+      else (if typeCheckFlag
+               then run untilTypecheck (handleSuccess "type-checked") inputName input verboseFlag
+               else run runFullPipeline handleSuccessAndWrite inputName input verboseFlag)
   where
-    fullCompilation :: String -> T.Text -> Bool -> IO ()
-    fullCompilation inputName input verboseFlag =
+    run :: Pipeline a -> SuccessHandler a -> String -> T.Text -> Bool -> IO ()
+    run pipeline successHandler inputName input verboseFlag =
       do
-        let compilationResult = runFullPipeline inputName (Text.unpack input)
-        either printError (handleSuccess inputName verboseFlag) compilationResult
-    parseAndTypecheck :: String -> T.Text -> Bool -> IO ()
-    parseAndTypecheck inputName input verboseFlag =
-      do
-        let compilationResult = runUntilTypecheck inputName (Text.unpack input)
-        either printError (handleTypeCheckSuccess inputName verboseFlag) compilationResult
-        -- if verboseFlag then printf ("Typed AST"%w%) compilationResult else return ()
+        let compilationResult = pipeline inputName (Text.unpack input)
+        either printError (successHandler inputName verboseFlag) compilationResult
 
 printError :: Error -> IO ()
 printError = eprintf (errorTextLeft%w%errorTextRight)
@@ -60,15 +60,15 @@ printError = eprintf (errorTextLeft%w%errorTextRight)
 type ClassFileName = String
 type Result = [(ClassFileName,ClassFile)]
 
-handleTypeCheckSuccess :: String -> Bool -> [(String,Class)] -> IO ()
-handleTypeCheckSuccess inputName verboseFlag results = do
-  printSuccessMessage inputName "type-checked"
+handleSuccess :: Show result => String -> SuccessHandler [(String,result)]
+handleSuccess jobName inputName verboseFlag results = do
+  printSuccessMessage inputName jobName
   if verboseFlag
     then mapM_ (\(name,res) -> printf("\nClass Name: "%w%"\nByte-code:\n"%w%"\n") name res) results
     else return ()
 
-handleSuccess :: String -> Bool -> Result -> IO ()
-handleSuccess inputName verboseFlag results = do
+handleSuccessAndWrite :: SuccessHandler Result
+handleSuccessAndWrite inputName verboseFlag results = do
   mapM_ (uncurry generateByteFile) results
   printSuccessMessage inputName "compiled"
   if verboseFlag
@@ -78,24 +78,30 @@ handleSuccess inputName verboseFlag results = do
 printSuccessMessage :: String -> String ->  IO ()
 printSuccessMessage inputName jobName = printf ("=== Success ===\n\n~~~\n"%s%" was successfully "%s%"!\n~~~\n") (Text.pack inputName) (Text.pack jobName)
 
-runUntilTypecheck :: String -> String -> Either Error [(String,Class)]
-runUntilTypecheck filePath input =
+untilParser :: Pipeline [(String,Class)]
+untilParser filePath input =
   do
     nonEmptyInput <- if null input
                        then Left $ ParseError "The input appears to be empty."
                        else return input
     parseResult <- parseSrc filePath  nonEmptyInput
-    typeCheckResult <- typecheck parseResult
-    return $ zip (map getClassName typeCheckResult) typeCheckResult
+    return $ zip (map getClassName parseResult) parseResult
   where
     getClassName :: Class -> ClassFileName
     getClassName (Class (Identifier className) _ _ ) = className ++ ".class"
     getClassName _ = error "Partial function"
 
-runFullPipeline :: String -> String -> Either Error Result
+untilTypecheck :: Pipeline [(String,Class)]
+untilTypecheck filePath input =
+  do
+    parseResult <- untilParser filePath input
+    typeCheckResult <- typecheck (map snd parseResult)
+    return $ zip (map fst parseResult) typeCheckResult
+
+runFullPipeline :: Pipeline Result
 runFullPipeline filePath input =
   do
-    typeCheckResult <- runUntilTypecheck filePath input
+    typeCheckResult <- untilTypecheck filePath input
     compilationResult <- mapM (compileByteCode . snd) typeCheckResult
     return $ zip (map fst typeCheckResult) compilationResult
   where
